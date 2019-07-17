d7626afc9185ae36bb32a54fe950d9823e79a482
EC2 Default User <ec2-user@ip-172-31-18-197.us-east-2.compute.internal>
2019-07-17 18:30:29 +0000

    looks kool m8

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,30 @@
 from app import app
 from flask import render_template, request
-from app.models import model, formopener
+from app.models import model, formopener, cypher
+# from templates import index
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    return render_template('index.html')
+    
+# @app.routes('/encode')
+# def encode():
+#     return render_template("index.html" ,name = name)
+    
+@app.route('/encode', methods = ["GET","POST"])
+def encode():
+    if request.method == "GET":
+        return "Please use the form"
+    else:
+        userdata = dict(request.form)
+        print(userdata)
+        # vre = userdata["breakfast"]
+        name = userdata["message"]
+        # breakfast = model.shout(vre.decode("utf-8"))
+        # return "under contruction"
+        return render_template('index.html', name = name)
+        
+# @app.route('/encoded')
+# def encoded():
+#     return
