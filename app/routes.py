3aef111f0dca75ac28347c03c0209d876636e9b9
EC2 Default User <ec2-user@ip-172-31-18-197.us-east-2.compute.internal>
2019-07-18 18:24:09 +0000

    added key

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template, request
 from app.models import model, formopener
 import math
+import random
 
 @app.route('/')
 @app.route('/index')
@@ -14,30 +15,50 @@ def encode():
     userdata = dict(request.form)
     name = (userdata["message"])
     finalString = ""
+    finalkey = ""
+    key = []
+    counter = 0
+    while counter < len(name):
+        new_value = random.randint(33,110)
+        key.append(new_value)
+        counter += 1
     for l in range(0, len(name)):
         letter = int(ord(name[l]))
-        pie = open("app/static/pie.txt").read()
-        if pie[l] == ".":
-            letter += 1
-        else:
-            letter += int(pie[l])
+        letter += int(key[l])
         finalString += chr(letter)
-    return render_template('index.html', name = name, finalString = finalString)
+    for x in key:
+        finalkey += chr(x)
+    return render_template('index.html', name = name, finalString = finalString, finalkey = finalkey)
 
 @app.route('/decode', methods = ["GET","POST"])
 def decrypt():
     if request.method == "GET":
         return render_template("decode.html")
     else:
+        finalString = ""
         userdata = dict(request.form)
         name = (userdata["encoded_message"])
-        finalString = ""
+        input_key = (userdata["input_key"])
         for l in range(0, len(name)):
             letter = int(ord(name[l]))
-            pie = open("app/static/pie.txt").read()
-            if pie[l] == ".":
-                letter -= 1
-            else:
-                letter -= int(pie[l])
+            letter -= int(ord(input_key[l]))
             finalString += chr(letter)
     return render_template('decode.html', name = name, finalString = finalString)
+    
+    
+    # def decrypt():
+    # if request.method == "GET":
+    #     return render_template("decode.html")
+    # else:
+    #     userdata = dict(request.form)
+    #     name = (userdata["encoded_message"])
+    #     finalString = ""
+    #     for l in range(0, len(name)):
+    #         letter = int(ord(name[l]))
+    #         pie = open("app/static/pie.txt").read()
+    #         if pie[l] == ".":
+    #             letter -= 1
+    #         else:
+    #             letter -= int(pie[l])
+    #         finalString += chr(letter)
+    # return render_template('decode.html', name = name, finalString = finalString)
